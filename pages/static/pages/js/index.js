9ce54b2f234d5f6b8323952e6cc70995441083c0
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2022-10-05 23:06:48 +0530

    10/5/22

--- pages/static/pages/js/index.js
+++ pages/static/pages/js/index.js
@@ -1,7 +1,7 @@
 function redirect(ele) {
             if(event.key === 'Enter') {
                 if(ele.value === 'LAKEHOUSE2022')
-                     window.location.href = 'https://drive.google.com/drive/u/0/my-drive/asset-v1-ANUx+ANU-ASTRO4x+2T2016+type@asset+block@A4_L1_V01.pdf';
+                     window.location.href = 'https://drive.google.com/drive/folders/1rcctKcHzSv1jlLmSh-1PWECFQ6XgtJFw';
                 else
                     alert("Invalid");
             }
