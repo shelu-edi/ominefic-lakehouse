620175593021ed135a0777189f37dd098dc86516
Shelumiel Ediriarachchi <61077989+shelu-edi@users.noreply.github.com>
2022-10-05 23:55:23 +0530

    :10/5/22

--- pages/static/pages/js/index.js
+++ pages/static/pages/js/index.js
@@ -1,6 +1,6 @@
 function redirect(ele) {
             if(event.key === 'Enter') {
-                if(ele.value === 'LAKEHOUSE2022')
+                if(ele.value === 'LAKE2022')
                      window.location.href = 'https://drive.google.com/drive/folders/1rcctKcHzSv1jlLmSh-1PWECFQ6XgtJFw';
                 else
                     alert("Invalid");
---
+++ static/pages/js/index.js
@@ -0,0 +1,8 @@
+function redirect(ele) {
+            if(event.key === 'Enter') {
+                if(ele.value === 'LAKE2022')
+                     window.location.href = 'https://drive.google.com/drive/folders/1rcctKcHzSv1jlLmSh-1PWECFQ6XgtJFw';
+                else
+                    alert("Invalid");
+            }
+        }
